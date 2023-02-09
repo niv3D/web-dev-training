3669c969fc29afbbeaed6d8b28f46efcf21b97b0
niv3D <57232335+niv3D@users.noreply.github.com>
2023-02-09 08:18:21 +0530

    update

--- java/HelloWorld/src/Browser.java
+++ java/HelloWorld/src/Browser.java
@@ -3,27 +3,24 @@ import javax.print.attribute.Size2DSyntax;
 public class Browser {
 
 	private static String[] history;
-	
+
 	public int arrayIndex = 0;
 	public static int size;
-	
-	
+
 	public Browser() {};
-	
-	
-	public static void setHistory(String[] newHistory,int length) {
+
+	public static void setHistory(String[] newHistory, int length) {
 		size = length;
 		history = new String[size];
 		System.arraycopy(newHistory, 0, history, 0, size);
 	}
-	
-	public Browser(String[] newHistory,int length) {
-			
+
+	public Browser(String[] newHistory, int length) {
+
 		size = length;
 		history = new String[size];
 		System.arraycopy(newHistory, 0, history, 0, size);
-		
+
 	}
-	
-	
+
 }
