10895e4fbc58b9c7faeede191b2f1f221f402897
niv3D <niv3d.username@gmail.com>
2023-02-15 12:28:25 +0530

    examples

---
+++ Java/Examples/Abstracts/src/GraphicObject.java
@@ -0,0 +1,16 @@
+
+abstract class GraphicObject {
+	int x,y;
+	
+	
+	void moveTo(int newX,int newY) {
+		
+		//.........
+		//.........
+		//.........
+	}
+	
+	abstract void draw();
+	abstract void resize();
+
+}
---
+++ Java/Examples/interface/src/InterfaceMethods.java
@@ -0,0 +1,24 @@
+class Horse {
+    public String identifyMyself() {
+        return "I am a horse.";
+    }
+}
+
+interface Flyer {
+    default public String identifyMyself() {
+        return "I am able to fly.";
+    }
+}
+
+interface Mythical {
+    default public String identifyMyself() {
+        return "I am a mythical creature.";
+    }
+}
+
+class InterfaceMethods extends Horse implements Flyer, Mythical {
+    public static void main(String... args) {
+        InterfaceMethods myApp = new InterfaceMethods();
+        System.out.println(myApp.identifyMyself());
+    }
+}
