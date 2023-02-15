c912ec9862c76a4371c10e636b3ecc9db98aacdb
niv3D <niv3d.username@gmail.com>
2023-02-15 16:38:54 +0530

    update google chrome

--- Java/Exercises/Exercise2/src/GoogleChrome.java
+++ Java/Exercises/Exercise2/src/GoogleChrome.java
@@ -1,6 +1,11 @@
 
 public class GoogleChrome extends Browser{
 	
+	private static boolean isLocationAccessible;
+	private static boolean isCameraAccessible;
+	private static boolean isMicrophoneAccessible;
+	
+	
 	GoogleChrome(String[] historyURL) {
 		
 		super(historyURL);
@@ -17,4 +22,36 @@ public class GoogleChrome extends Browser{
 		System.out.println("I am Google Chrome");
 	}
 	
+	public static void setPermissions(String access) {
+		
+		if(access.equals("camera")) isCameraAccessible = true;
+		if(access.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access.equals("Location")) isLocationAccessible = true;
+	}
+	
+	public static void setPermissions(String access1,String access2) {
+		
+		if(access1.equals("camera")) isCameraAccessible = true;
+		if(access1.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access1.equals("Location")) isLocationAccessible = true;
+		
+		if(access2.equals("camera")) isCameraAccessible = true;
+		if(access2.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access2.equals("Location")) isLocationAccessible = true;
+	}
+	
+	public static void setPermissions(String access1,String access2,String access3) {
+		
+		if(access1.equals("camera")) isCameraAccessible = true;
+		if(access1.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access1.equals("Location")) isLocationAccessible = true;
+		
+		if(access2.equals("camera")) isCameraAccessible = true;
+		if(access2.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access2.equals("Location")) isLocationAccessible = true;
+		
+		if(access3.equals("camera")) isCameraAccessible = true;
+		if(access3.equals("Microphone")) isMicrophoneAccessible = true;
+		if(access3.equals("Location")) isLocationAccessible = true;
+	}
 }
