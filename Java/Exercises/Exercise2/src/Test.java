4faf322220f21c16cb46d374defc83817dcceca1
niv3D <niv3d.username@gmail.com>
2023-02-16 12:19:33 +0530

    Create Test

---
+++ Java/Exercises/Exercise2/src/Test.java
@@ -0,0 +1,33 @@
+import java.nio.channels.InterruptedByTimeoutException;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Browser tabOne = new GoogleChrome();
+		Browser tabTwo = new Firefox();
+		Browser tabThree = new Firefox();
+		Browser tabFour= new GoogleChrome();
+		Browser tabFive= new GoogleChrome();
+		Browser[] allBrowsers = new Browser[5];
+		
+		allBrowsers[0] = tabOne;
+		allBrowsers[1] = tabTwo;
+		allBrowsers[2] = tabThree;
+		allBrowsers[3] = tabFour;
+		allBrowsers[4] = tabFive;
+		
+		int chromeTabs = 0;
+		
+		
+		for(Browser tab: allBrowsers ) {
+			
+			Class a = tab.getClass();
+			
+			//System.out.println(a.getName());
+			
+			if(a.getName().equals("GoogleChrome")) chromeTabs++;
+		}
+		
+	}
+
+}
