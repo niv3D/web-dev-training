92d4a499cc6d64548c950a0a6a7e3b5577502104
niv3D <niv3d.username@gmail.com>
2023-02-17 11:52:44 +0530

    containers

--- Java/Exercises/Exercise2/src/Firefox.java
+++ Java/Exercises/Exercise2/src/Firefox.java
@@ -1,5 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Firefox extends Browser implements MultipleAccountContainers{
+	
+	private Set<String> containerSet = new HashSet<>();
+	
 	
 	Firefox(String[] historyURL) {
 		
@@ -14,5 +19,31 @@ public class Firefox extends Browser implements MultipleAccountContainers{
 	@Override
 	public void whoAmI() {
 		System.out.println("I am Firefox");
+	}
+
+	@Override
+	public void addContainer(String name) {
+		
+		if (!containerSet.contains(name)) {
+			
+			containerSet.add(name);
+			return;
+		}
+		
+		System.out.println("Container already exist !");
+	
+	}
+
+	@Override
+	public void leaveContainer(String name) {
+		
+		if (containerSet.contains(name)) {
+			
+			containerSet.remove(name);
+			return;
+		}
+		
+		System.out.println("Container : "+ name + " do not exist");
+		
 	}
 }
--- Java/Exercises/Exercise2/src/MultipleAccountContainers.java
+++ Java/Exercises/Exercise2/src/MultipleAccountContainers.java
@@ -1,6 +1,8 @@
 
 public interface MultipleAccountContainers {
 	
+	public void addContainer(String name);
+	public void leaveContainer(String name);
 	
 
 }
