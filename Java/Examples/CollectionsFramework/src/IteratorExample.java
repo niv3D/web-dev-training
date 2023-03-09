0ae1e5b6142fda49301eb4d1edd606eafb5ac19a
niv3D <niv3d.username@gmail.com>
2023-03-10 02:39:37 +0530

    collection

--- Java/Examples/CollectionsFramework/src/IteratorExample.java
+++ Java/Examples/CollectionsFramework/src/IteratorExample.java
@@ -1,38 +1,70 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class IteratorExample {
 
 	public static void main(String[] args) {
-		
-		ArrayList<String> listOfStrings = new ArrayList<>();
-		
-		listOfStrings.add("zero");
-		listOfStrings.add("one");
-		listOfStrings.add("two");
-		listOfStrings.add("three");
-		listOfStrings.add("four");
-		listOfStrings.add("five");
-		
-		
-		
+
+		ArrayList<String> listOfStrings = new ArrayList<>(Arrays.asList("one", "two", "three", "four", "five"));
+
 		for (Iterator<String> iterator = listOfStrings.iterator(); iterator.hasNext();) {
-			
+
 			String element = iterator.next();
 			if (element.equals("three")) {
-				
+				System.out.println(element);
 			}
-			System.out.println(element);
-			
+
 		}
 		
 		
-		ArrayList<String> strings = new ArrayList<>(Arrays.asList("a","b","c"));
-		
+		listOfStrings.subList(1, 3).clear();
+
+		ArrayList<String> strings = new ArrayList<>(Arrays.asList("a", "b", "c"));
+
 		String aChar = strings.get(0);
- 	}
-	 
+		
+		
+		
+		class Employee {
+
+			public int age;
+
+			public Employee(int age) {
+				this.age = age;
+			}
+
+		}
+
+		class AgeSorter implements Comparator<Employee> {
+
+			@Override
+			public int compare(Employee e1, Employee e2) {
+				if (e1.age < e2.age) {
+					return -1;
+				} else if (e1.age > e2.age) {
+					return 1;
+				} else {
+					return 0;
+				}
+			}
+
+		}
+		
+		Comparator<Employee> newSorter = (e1,e2)->{ return -1;};
+		
+		ArrayList<Employee> employees = new ArrayList<>();
+		
+		
+		employees.add(new Employee(23));
+		employees.add(new Employee(28));
+		employees.add(new Employee(19));
+		
+		
+		employees.sort(new AgeSorter());
+	}
+
 	
-	
+
 }
