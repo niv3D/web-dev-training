1252b675b8ea417f48bccb5cde6eb9654fef67ee
niv3D <niv3d.username@gmail.com>
2023-03-16 13:30:18 +0530

    reflection

--- Java/Examples/ReflectionAPI/src/CarData.java
+++ Java/Examples/ReflectionAPI/src/CarData.java
@@ -3,12 +3,29 @@ public class CarData {
 	
 	public final int gears;
 	private int currentGear;
+	private int speed;
 	public String carName;
-	public static String type;
 	
 	
 	public CarData(int gears, String carName) {
 		this.gears = gears;
 		this.carName = carName;
 	}
+
+	public int getCurrentGear() {
+		return currentGear;
+	}
+
+	public void setCurrentGear(int currentGear) {
+		this.currentGear = currentGear;
+	}
+	
+	public String getcarName() {
+		return carName;
+	}
+	
+	private void setSpeed(int initial ,int acceleration , int duration) {
+		speed = initial + acceleration * duration ; 
+	}
+	
 }
--- Java/Examples/ReflectionAPI/src/Main.java
+++ Java/Examples/ReflectionAPI/src/Main.java
@@ -1,32 +1,64 @@
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+import java.util.Iterator;
 
 public class Main {
 
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException {
-		
-		
-		String randomString =  "abcdef";
-		
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, SecurityException, NoSuchMethodException {
+
+		String randomString = "abcdef";
+
+		CarData newCarData = new CarData(5, randomString);
+
 		Class<? extends String> stringClass = randomString.getClass();
-		
-		Class<String> classObject  = String.class;
-		
+
+		Class<String> classObject = String.class;
+
 		System.out.println(classObject.getName());
-		
+
 		Class.forName("java.lang.String");
-		
+
 		Class<CarData> carClass = CarData.class;
-		
-		
+
 		Field[] carFields = carClass.getDeclaredFields();
-		
-		for(Field field : carFields) {
+
+		for (Field field : carFields) {
 			System.out.println(field.getName());
 		}
-		
+
 		Field currentGearField = carClass.getDeclaredField("currentGear");
-		
+
 		currentGearField.setAccessible(true);
-	}
+
+		try {
+			currentGearField.set(newCarData, 10);
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 		
+		System.out.println("/////////////");
+		
+		Constructor<?>[] carConstructor = carClass.getConstructors();
+		
+		for (Constructor<?> constructor : carConstructor) {
+			
+		
+		   Type[] types = constructor.getGenericParameterTypes(); 
+		   
+		  Parameter[] parameters = constructor.getParameters();
+		   
+		  for(int i = 0 ; i < parameters.length ; i++) {
+			  
+			  System.out.println(parameters[i].getName() + ' ' + types[i].getTypeName());
+			  
+		  }
+		
+		}
+	
+		
+		
+	}
+
 }
